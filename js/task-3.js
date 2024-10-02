bc6575f6d1c3ca9ac06397c451bdb11a2a3364ab
Nikkkitos <nikitosden807@gmail.com>
2024-10-02 21:27:46 +0300

    hw-06

--- js/task-3.js
+++ js/task-3.js
@@ -1,24 +1,33 @@
-const profile = {
-  username: "Jacob",
-  playTime: 300,
+class StringBuilder {
+  #value;
 
-  changeUsername(newName) {
-    this.username = newName;
-  },
-
-  updatePlayTime(hours) {
-    this.playTime += hours;
-  },
-
-  getInfo() {
-    return `${this.username} has ${this.playTime} active hours!`;
+  constructor(initialValue) {
+    this.#value = initialValue;
   }
-};
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+  getValue() {
+    return this.#value;
+  }
 
-profile.changeUsername("Marco");
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+  padEnd(str) {
+    this.#value += str;
+  }
 
-profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+  padStart(str) {
+    this.#value = str + this.#value;
+  }
+
+  padBoth(str) {
+    this.padStart(str);
+    this.padEnd(str);
+  }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
